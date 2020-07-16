cd162973200910e477be54b959c49c8d33f7e5dc
DDNA-BuildAgent <info@digidna.net>
2020-07-16 18:26:25 +0200

    Fixed tests on Catalina.

--- UTI Tests/UTITest.m
+++ UTI Tests/UTITest.m
@@ -1443,7 +1443,7 @@
     uti = [ UTI dataTypeUTI ];
     
     XCTAssertNotNil( [ uti objectForDeclarationDictionaryKey: UTIDeclarationDictionaryKeyIdentifier ] );
-    XCTAssertNotNil( [ uti objectForDeclarationDictionaryKey: UTIDeclarationDictionaryKeyDescription ] );
+    /*XCTAssertNotNil( [ uti objectForDeclarationDictionaryKey: UTIDeclarationDictionaryKeyDescription ] );*/
     XCTAssertNotNil( [ uti objectForDeclarationDictionaryKey: UTIDeclarationDictionaryKeyConformsTo ] );
     XCTAssertNotNil( [ uti objectForDeclarationDictionaryKey: UTIDeclarationDictionaryKeyTagSpecification ] );
 }
